2d9ac794521d3065f7780462d16f7dd4da80b2db
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-10 01:53:18 -0700

    2 files modified

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MyComponent = () => {
-  const [count, setCount] = useState(0);
   const [date, setDate] = useState(new Date());
-
-  const handleClick = () => {
-    setCount(prevState => prevState + 1);
-  };
 
   const tick = () => {
     setDate(new Date());
   };
 
+  useEffect(() => {
+    setInterval(tick, 1000);
+  }, []);
+
   return (
     <div>
       <p>Time: {date.toLocaleTimeString()}</p>
-      <button type="button" onClick={handleClick}>
-        Click
-      </button>
     </div>
   );
 };
+
+export default MyComponent;
